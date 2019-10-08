6264f6190919e7c46a6ea2a7e8d1c5fd09f48b48
WenHong <lauwy044@mymail.unisa.edu.au>
2019-10-09 01:07:57 +1030

    added methods create and update to make it writable

--- cities/serializer.py
+++ cities/serializer.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
+from .city import City
 
 
 class CitySerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField(max_length=100)
     csvname = serializers.CharField(max_length=256)
+
+    def create(self, validated_data):
+        return City(id=None, **validated_data)
+
+    def update(self, instance, validated_data):
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+        return instance
