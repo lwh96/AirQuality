d576ce207131dbda8e225213e8499e5cacd2d455
WenHong <lauwy044@mymail.unisa.edu.au>
2019-10-09 01:48:42 +1030

    added method to calculate standard deviation of air measurements

--- cities/views.py
+++ cities/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
+import pandas as pd
+
 from .city import cities
 from .serializer import CitySerializer
 
@@ -16,3 +18,20 @@ class CityViewSet(viewsets.ViewSet):
         print(cities)
         serializer = CitySerializer(instance=cities[pk], many=False)
         return Response(serializer.data)
+
+    # Util Functions
+    def calc_std_dev(self, filename):
+        df = pd.read_csv(filename)
+
+        measures = ['PM2.5', 'PM10', 'SO2', 'NO2', 'CO', 'O3']
+
+        data = {}
+
+        for measure in measures:
+            dfgt20 = df.dropna(subset=[measure, 'TEMP'], how='any').query('TEMP > 20')
+            dflte20 = df.dropna(subset=[measure, 'TEMP'], how='any').query('TEMP <= 20')
+            data[measure] = {}
+            data[measure]['Above 20'] = dfgt20[measure].std()
+            data[measure]['20 and Below'] = dflte20[measure].std()
+
+        return data
