6589d629033ee0173665092b25645fe6dc2c5385
WenHong <lauwy044@mymail.unisa.edu.au>
2019-10-09 01:02:18 +1030

    added city class, initialising all 12 cities

---
+++ cities/city.py
@@ -0,0 +1,27 @@
+class City(object):
+    def __init__(self, **kwargs):
+        for field in ('id', 'name', 'csvname'):
+            setattr(self, field, kwargs.get(field, None))
+
+
+city_names = [
+    'Aotizhongxin',
+    'Changping',
+    'Dingling',
+    'Dongsi',
+    'Guanyuan',
+    'Gucheng',
+    'Huairou',
+    'Nongzhanguan',
+    'Shunyi',
+    'Tiantan',
+    'Wanliu',
+    'Wanshouxigong'
+]
+
+CSV_FILE_FORMAT = 'air/csv/PRSA_Data_{}_20130301-20170228.csv'
+cities = {}
+for index in range(len(city_names)):
+    cities[str(index + 1)] = City(id=index + 1,
+                                  name=city_names[index],
+                                  csvname=CSV_FILE_FORMAT.format(city_names[index]))
