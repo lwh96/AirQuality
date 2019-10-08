5d889e6a9438cea524356219312805a888dd320f
WenHong <lauwy044@mymail.unisa.edu.au>
2019-10-09 01:12:29 +1030

    added viewset for city object to list all cities

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .city import cities
+from .serializer import CitySerializer
 
-# Create your views here.
+
+class CityViewSet(viewsets.ViewSet):
+    serializer_class = CitySerializer
+
+    def list(self, request):
+        serializer = CitySerializer(
+            instance=cities.values(), many=True)
+        return Response(serializer.data)
